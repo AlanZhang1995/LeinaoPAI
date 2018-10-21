1122468c20ee841dc8a82a363ea234f259c40968
zhc <32972595+AlanZhang1995@users.noreply.github.com>
2018-10-21 21:08:36 +0800

    Add files via upload

---
+++ images/temporal-segment-networks2/Readme.md
@@ -0,0 +1,12 @@
+# Docker image for Temporal Segment Networks
+
+特性
+=============
+> * OpenCV
+> * Caffe
+> * DenseFlow
+> * TSN codebase
+> * MPI multi-GPU training
+
+=============
+http://yjxiong.me/others/tsn/
---
+++ images/temporal-segment-networks2/train_tsn.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+DATASET=$1
+MODALITY=$2
+
+TOOLS=lib/caffe-action/build/install/bin
+LOG_FILE=/userhome/TSN/logs/multi-${DATASET}_${MODALITY}_split1.log
+N_GPU=4
+MPI_BIN_DIR=/userhome/TSN/MPI/mpich/bin/
+
+
+echo "logging to ${LOG_FILE}"
+
+${MPI_BIN_DIR}mpirun --allow-run-as-root  -np $N_GPU \
+$TOOLS/caffe train --solver=/userhome/TSN/files/${DATASET}/tsn_bn_inception_${MODALITY}_solver.prototxt  \
+   --weights=models/bn_inception_${MODALITY}_init.caffemodel 2>&1 | tee ${LOG_FILE}
